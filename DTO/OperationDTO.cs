44b0b43a338aad1baaa778a90ed11f0c530f4980
lahlousane <59821323+lahlousane@users.noreply.github.com>
2020-03-20 13:50:03 +0100

    Update OperationDTO.cs

--- DTO/OperationDTO.cs
+++ DTO/OperationDTO.cs
@@ -1,14 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CalculatriceAPI.DTO
 {
+    using Models;
     public class OperationDTO
     {
         public int Id { get; set; }
         public string Nom { get; set; }
         public string Valeur { get; set; }
+        public UtilisateurDTO Auteur { get; set; }
+
+        public OperationDTO() { }
+
+        public OperationDTO(int id, string nom, string valeur, UtilisateurDTO auteur)
+        {
+            Id = id;
+            Nom = nom;
+            Valeur = valeur;
+            Auteur = auteur;
+        }
+
+        public static implicit operator Operation (OperationDTO op)
+        {
+            return new Operation(op.Id, op.Nom, op.Valeur, op.Auteur);
+        }
+
     }
 }
